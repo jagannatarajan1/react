8f77ec0248a18289819c69d4f70f037fd76763b2
jagannatarajan1 <jagannatarajan1@gmail.com>
2023-11-20 21:05:05 +0530

    added the  dynamic pass the value to the location of expenditure

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,9 +1,30 @@
 import ExpenseItems from "./components/ExpenseItem";
 function App() {
+  const obj = [
+    { LocationOfExpenditure: "basic needs" },
+    { LocationOfExpenditure: "essential needs" },
+    { LocationOfExpenditure: "not essential needs" },
+    { LocationOfExpenditure: " needs" },
+    { LocationOfExpenditure: "no needs" },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItems></ExpenseItems>
+      <ExpenseItems
+        LocationOfExpenditure={obj[0].LocationOfExpenditure}
+      ></ExpenseItems>
+      <ExpenseItems
+        LocationOfExpenditure={obj[1].LocationOfExpenditure}
+      ></ExpenseItems>
+      <ExpenseItems
+        LocationOfExpenditure={obj[2].LocationOfExpenditure}
+      ></ExpenseItems>
+      <ExpenseItems
+        LocationOfExpenditure={obj[3].LocationOfExpenditure}
+      ></ExpenseItems>
+      <ExpenseItems
+        LocationOfExpenditure={obj[4].LocationOfExpenditure}
+      ></ExpenseItems>
     </div>
   );
 }
--- react-basic/src/components/ExpenseItem.js
+++ react-basic/src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import "./ExpenseItems.css";
-function ExpenseItems() {
+function ExpenseItems(props) {
   const ExpenseDate = new Date();
   const Expense = "Food";
   const Expenseprice = 10;
-  const LocationOfExpenditure = "Basic Needs";
   return (
     <div className="expense-item">
       <div>{ExpenseDate.toString()}</div>
       <div className="expense-items_description">
         <h2>{Expense}</h2>
       </div>
-      <h2>{LocationOfExpenditure}</h2>
+      <h2>{props.LocationOfExpenditure}</h2>
       <div className="expense-item_price">${Expenseprice}</div>
     </div>
   );
