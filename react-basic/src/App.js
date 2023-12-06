b810b54976757e6d9d9e97fc702accd107e27359
jagannatarajan1 <jagannatarajan1@gmail.com>
2023-12-06 19:41:58 +0530

    all changes

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,40 +1,52 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import NewExpenseForm from "./components/expense/NewExpense/NewExpenseForm";
-import ExpenseDetails from "./components/expense/ExpenseDetails";
-function App() {
-  const obj = [
-    {
-      id: 1,
-      LocationOfExpenditure: "basic needs",
-      title: "Food",
-      amount: 1,
-    },
-    {
-      id: 2,
-      LocationOfExpenditure: "essential needs",
-      title: "Food",
-      amount: 12,
-    },
-    {
-      id: 3,
-      LocationOfExpenditure: "not essential needs",
-      title: "Food",
-      amount: 13,
-    },
-    { id: 4, LocationOfExpenditure: " needs", title: "Food", amount: 14 },
-    { id: 5, LocationOfExpenditure: "no needs", title: "Food", amount: 15 },
-  ];
-  const ExpenseFromNewExpenseForm = (expense) => {
-    console.log(expense);
-    console.log("this is the expense form the list of app js");
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <NewExpenseForm onAddExpense={ExpenseFromNewExpenseForm} />
-      <ExpenseDetails items={obj} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
