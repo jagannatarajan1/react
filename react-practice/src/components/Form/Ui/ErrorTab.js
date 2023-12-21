3ba07bd5355ad8f79441cd036846b7f280d2f7b3
jagannatarajan1 <jagannatarajan1@gmail.com>
2023-12-21 13:35:41 +0530

    createPortal functionality used

--- react-practice/src/components/Form/Ui/ErrorTab.js
+++ react-practice/src/components/Form/Ui/ErrorTab.js
@@ -1,15 +1,39 @@
 import React from "react";
 import Button from "../Button/Button";
 import "./ErrorTab.css";
+import ReactDom from "react-dom";
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.outtab}></div>;
+};
+const ModelOverlay = (props) => {
+  return (
+    <React.Fragment>
+      <div className="modal">
+        <header className="header">{props.title}</header>
+        <p className="content">{props.message}</p>
+        <footer className="actions">
+          <Button onClick={props.outtab}>okay</Button>
+        </footer>
+      </div>
+    </React.Fragment>
+  );
+};
 const ErrorTab = (props) => {
   return (
-    <div className="modal" onClick={props.outtab}>
-      <header className="header">{props.title}</header>
-      <p className="content">{props.message}</p>
-      <footer className="actions">
-        <Button onClick={props.outtab}>okay</Button>
-      </footer>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop outtab={props.outtab} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModelOverlay
+          title={props.title}
+          message={props.message}
+          outtab={props.outtab}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 export default ErrorTab;
